4f791c26bf51f7dafc1a7950656cd6c4ce32ceeb
GioVargas <47228026+GioVargas@users.noreply.github.com>
2020-08-18 10:59:12 -0500

    Add files via upload

---
+++ english/ingles.cpp
@@ -0,0 +1,195 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "ingles.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+float punt_part1;
+bool empezar;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image1Click(TObject *Sender)
+{
+punt_part1= 0;
+Panel1->Visible=true;
+Panel2->Visible=true;
+Panel3->Visible=true;
+Button1->Visible=true;
+Edit1->Visible=true;
+Panel4->Visible=false;
+Panel5->Visible=false;
+Panel6->Visible=false;
+Button2->Visible=false;
+Edit2->Visible=false;
+Image3->Visible=false;
+Image4->Visible=false;
+Image5->Visible=false;
+Image6->Visible=false;
+Image7->Visible=false;
+Image8->Visible=false;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image2Click(TObject *Sender)
+{
+punt_part1= 0;
+Panel1->Visible=false;
+Panel2->Visible=false;
+Panel3->Visible=false;
+Button1->Visible=false;
+Edit1->Visible=false;
+Image3->Visible=false;
+Image4->Visible=false;
+Image5->Visible=false;
+Image6->Visible=false;
+Image7->Visible=false;
+Image8->Visible=false;
+Panel4->Visible=true;
+Panel5->Visible=true;
+Panel6->Visible=true;
+Button2->Visible=true;
+Edit2->Visible=true;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Timer1Timer(TObject *Sender)
+{
+StatusBar1->Panels->Items[0]->Text=Now().DateTimeString();
+StatusBar1->Panels->Items[1]->Text= "G.A.B.I.SOFT ®" ;
+StatusBar1->Panels->Items[2]->Text="Modulo de Ingles, Listening y ejercicios";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+   if ((RadioButton5->Checked==true))
+  {
+   punt_part1=punt_part1 + 1;
+   Image3->Visible=true;
+   Panel1->Enabled=false;
+    }
+   else
+   {
+     Image4->Visible=true;
+   Panel1->Enabled=false;
+    punt_part1=punt_part1;
+   }
+
+      if ((RadioButton1->Checked==true))
+  {
+   punt_part1=punt_part1 + 1;
+   Image7->Visible=true;
+   Panel2->Enabled=false;
+    }
+   else
+   {
+     Image8->Visible=true;
+   Panel2->Enabled=false;
+    punt_part1=punt_part1;
+   }
+
+   if ((RadioButton12->Checked==true))
+  {
+   punt_part1=punt_part1 + 1;
+   Image5->Visible=true;
+   Panel3->Enabled=false;
+    }
+   else
+   {
+     Image6->Visible=true;
+   Panel3->Enabled=false;
+    punt_part1=punt_part1;
+   }
+   Edit1->Text=FloatToStr(punt_part1)+" de 3";
+   Image1->Visible=False;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+   if ((RadioButton7->Checked==true))
+  {
+   punt_part1=punt_part1 + 1;
+   Image3->Visible=true;
+   Panel4->Enabled=false;
+    }
+   else
+   {
+     Image4->Visible=true;
+   Panel4->Enabled=false;
+    punt_part1=punt_part1;
+   }
+
+      if ((RadioButton19->Checked==true))
+  {
+   punt_part1=punt_part1 + 1;
+   Image7->Visible=true;
+   Panel5->Enabled=false;
+    }
+   else
+   {
+     Image8->Visible=true;
+   Panel5->Enabled=false;
+    punt_part1=punt_part1;
+   }
+
+   if ((RadioButton21->Checked==true))
+  {
+   punt_part1=punt_part1 + 1;
+   Image5->Visible=true;
+   Panel6->Enabled=false;
+    }
+   else
+   {
+     Image6->Visible=true;
+   Panel6->Enabled=false;
+    punt_part1=punt_part1;
+   }
+   Edit2->Text=FloatToStr(punt_part1)+" de 3";
+   Image2->Visible=False;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image9Click(TObject *Sender)
+{
+Close();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FileListBox1DblClick(TObject *Sender)
+{
+        MediaPlayer1->FileName = FileListBox1->FileName;
+        MediaPlayer1->Open();
+        MediaPlayer1->Play();
+        ProgressBar1->Position = 0;
+        ProgressBar1->Max = MediaPlayer1->Length;
+        empezar = true;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Timer2Timer(TObject *Sender)
+{
+if (empezar == true);
+	{
+		ProgressBar1->Position = MediaPlayer1->Position;
+		if(ProgressBar1->Position == ProgressBar1->Max)
+		{
+			if(FileListBox1->ItemIndex<FileListBox1->Items->Count)
+			{
+				FileListBox1->ItemIndex = FileListBox1->ItemIndex +1;
+				MediaPlayer1->FileName = FileListBox1->FileName;
+				
+	}
+		}
+			}        
+}
+//---------------------------------------------------------------------------
---
+++ english/ingles.~h
@@ -0,0 +1,115 @@
+//---------------------------------------------------------------------------
+
+#ifndef inglesH
+#define inglesH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <jpeg.hpp>
+#include <Buttons.hpp>
+#include <Graphics.hpp>
+#include <ComCtrls.hpp>
+#include <FileCtrl.hpp>
+#include <MPlayer.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TImage *Fondo;
+        TTimer *Timer1;
+        TPanel *Panel2;
+        TRadioButton *RadioButton1;
+        TRadioButton *RadioButton2;
+        TRadioButton *RadioButton3;
+        TRadioButton *RadioButton4;
+        TLabel *Label1;
+        TLabel *Label2;
+        TLabel *Label3;
+        TLabel *Label4;
+        TLabel *Label5;
+        TPanel *Panel1;
+        TLabel *Label6;
+        TLabel *Label7;
+        TLabel *Label9;
+        TLabel *Label10;
+        TRadioButton *RadioButton5;
+        TRadioButton *RadioButton6;
+        TRadioButton *RadioButton8;
+        TPanel *Panel3;
+        TLabel *Label11;
+        TLabel *Label12;
+        TLabel *Label13;
+        TLabel *Label14;
+        TLabel *Label15;
+        TRadioButton *RadioButton9;
+        TRadioButton *RadioButton10;
+        TRadioButton *RadioButton11;
+        TRadioButton *RadioButton12;
+        TImage *Image1;
+        TImage *Image2;
+        TPanel *Panel4;
+        TLabel *Label8;
+        TLabel *Label16;
+        TLabel *Label17;
+        TLabel *Label18;
+        TRadioButton *RadioButton7;
+        TRadioButton *RadioButton13;
+        TRadioButton *RadioButton14;
+        TPanel *Panel5;
+        TLabel *Label20;
+        TLabel *Label21;
+        TLabel *Label22;
+        TLabel *Label23;
+        TLabel *Label24;
+        TRadioButton *RadioButton16;
+        TRadioButton *RadioButton17;
+        TRadioButton *RadioButton18;
+        TRadioButton *RadioButton19;
+        TPanel *Panel6;
+        TLabel *Label25;
+        TLabel *Label26;
+        TLabel *Label27;
+        TLabel *Label28;
+        TLabel *Label29;
+        TRadioButton *RadioButton20;
+        TRadioButton *RadioButton21;
+        TRadioButton *RadioButton22;
+        TRadioButton *RadioButton23;
+        TStatusBar *StatusBar1;
+        TImage *Image3;
+        TImage *Image4;
+        TImage *Image5;
+        TImage *Image6;
+        TImage *Image7;
+        TImage *Image8;
+        TButton *Button1;
+        TButton *Button2;
+        TEdit *Edit1;
+        TEdit *Edit2;
+        TImage *Image9;
+        TFileListBox *FileListBox1;
+        TDirectoryListBox *DirectoryListBox1;
+        TDriveComboBox *DriveComboBox1;
+        TFilterComboBox *FilterComboBox1;
+        TProgressBar *ProgressBar1;
+        TMediaPlayer *MediaPlayer1;
+        TTimer *Timer2;
+        void __fastcall Image1Click(TObject *Sender);
+        void __fastcall Image2Click(TObject *Sender);
+        void __fastcall Timer1Timer(TObject *Sender);
+        void __fastcall Button1Click(TObject *Sender);
+        void __fastcall Button2Click(TObject *Sender);
+        void __fastcall Image9Click(TObject *Sender);
+        void __fastcall FileListBox1DblClick(TObject *Sender);
+        void __fastcall Timer2Timer(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
